69f59ade29c99f3328ba2c9139d4ce1bba0d073c
Jose Francisco JFPS. Patino Solis <jose.patino@stkrursus.com>
2021-02-03 08:41:31 -0600

    Ahi esta el proyecto, dense

---
+++ ceteq/src/test/java/com/ceteq/CeteqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceteq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeteqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
