f56a8e734160dafe55eb24f5c8999b0853a47948
bonAngeLOL <neomodlco@gmail.com>
2021-02-05 01:18:19 -0600

    Aspirante Impl: AspiranteModel changed to AspiranteBean

--- sistema/src/main/java/com/ceteq/sistema/serviceImpl/AspiranteImpl.java
+++ sistema/src/main/java/com/ceteq/sistema/serviceImpl/AspiranteImpl.java
@@ -1,9 +1,11 @@
 package com.ceteq.sistema.serviceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import com.ceteq.sistema.bean.AspiranteBean;
 import com.ceteq.sistema.model.AspiranteModel;
 import com.ceteq.sistema.repository.AspiranteRepository;
 import com.ceteq.sistema.service.AspiranteService;
@@ -29,20 +31,64 @@ public class AspiranteImpl implements AspiranteService{
     }
 
     @Override
-    public List<AspiranteModel> findAll() {
+    public List<AspiranteBean> findAll() {
         List<AspiranteModel> aspirantes = aspiranteRep.findAll();
-        return aspirantes;
+        List<AspiranteBean> aspiranteBL = new ArrayList<>();
+        for(AspiranteModel asp : aspirantes){
+            AspiranteBean aspB = new AspiranteBean(
+                    asp.getIdAlumno(), 
+                    asp.getNombre(), 
+                    asp.getApP(), 
+                    asp.getApM(), 
+                    asp.getEdad(), 
+                    asp.getDireccion(),
+                    asp.getCiudad(), 
+                    asp.getTelefono(), 
+                    asp.getCorreo(), 
+                    asp.getFechaRegistro(),
+                    asp.getFechaBaja());
+            aspiranteBL.add(aspB);
+        }
+        return aspiranteBL;
     }
 
     @Override
-    public AspiranteModel findById(int id) {
+    public AspiranteBean findById(int id) {
         AspiranteModel aspM = aspiranteRep.findById(id).orElseThrow();
-        return aspM;
+
+        AspiranteBean aspB = new AspiranteBean(
+               aspM.getIdAlumno(), 
+               aspM.getNombre(), 
+               aspM.getApP(), 
+               aspM.getApM(), 
+               aspM.getEdad(), 
+               aspM.getDireccion(), 
+               aspM.getCiudad(), 
+               aspM.getTelefono(), 
+               aspM.getCorreo(), 
+               aspM.getFechaRegistro(), 
+               aspM.getFechaBaja());
+        return aspB;
     }
 
     @Override
-    public void save(AspiranteModel aspirante) {
-        aspiranteRep.save(aspirante);
+    public boolean save(AspiranteBean aspiranteB) {
+        AspiranteModel aspM = new AspiranteModel(
+                aspiranteB.getIdAlumno(), 
+                aspiranteB.getNombre(), 
+                aspiranteB.getApP(), 
+                aspiranteB.getApM(), 
+                aspiranteB.getEdad(), 
+                aspiranteB.getDireccion(), 
+                aspiranteB.getCiudad(), 
+                aspiranteB.getTelefono(),
+                aspiranteB.getCorreo(), 
+                aspiranteB.getFechaRegistro(), 
+                aspiranteB.getFechaBaja()
+        );
+
+        aspiranteRep.save(aspM);
+        return true;
     }
 
 } 
