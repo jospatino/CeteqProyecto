0e98e44a5f207d0ab13ff3238928a566b5559b96
bonanAngeLOL <angellopezcue@gmail.com>
2021-02-10 19:57:56 -0600

    Interface Projection

---
+++ sistema/src/main/java/com/ceteq/sistema/interface_projection/AspiranteDeudaProjection.java
@@ -0,0 +1,15 @@
+package com.ceteq.sistema.interface_projection;
+
+public interface AspiranteDeudaProjection{
+
+    Integer getIdAlumno();
+
+    String getNombre();
+
+    String getApellidoPaterno();
+
+    String getApellidoMaterno();
+
+    Double getDeuda();
+
+}
