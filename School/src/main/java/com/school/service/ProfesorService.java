c3d2337efafad9d4864eddb11cd7690eb3b1dd09
Daniel <daniel.hernandez8020@gmail.com>
2021-02-23 13:03:13 -0600

    Profesor service

--- School/src/main/java/com/school/service/ProfesorService.java
+++ School/src/main/java/com/school/service/ProfesorService.java
@@ -1,5 +1,29 @@
 package com.school.service;
 
+import java.util.List;
+
+import com.school.bean.ProfesorBean;
+
+
+
 public interface ProfesorService {
+	
+	
+	
+	//create integer objbean
+		public Integer saveProfesor(ProfesorBean ProfesorIngresado);
+		
+		//read list de bean y objetobean
+		public List<ProfesorBean> Profesorlist(); //muestra la lista
+		
+		public ProfesorBean ProfesorbyId(Integer id_profesor); //devuelve alumno por id
+		
+		//update devuelve true
+		public boolean uptdateProfesor(Integer id_Producto);
+		
+		//deleted by id
+
+		public boolean deleteProfesor(Integer id_alumno);
+	
 
 }
