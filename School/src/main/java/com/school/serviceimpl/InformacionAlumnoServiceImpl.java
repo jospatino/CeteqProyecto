93b42493928b9518cffaf4d3185b90fdb100fe81
Jeremy <eremy95.ramos@gmail.com>
2021-02-23 13:26:09 -0600

    cambios actualizados

--- School/src/main/java/com/school/serviceimpl/InformacionAlumnoServiceImpl.java
+++ School/src/main/java/com/school/serviceimpl/InformacionAlumnoServiceImpl.java
@@ -1,7 +1,40 @@
 package com.school.serviceimpl;
 
+import java.util.List;
+
+import com.school.bean.InformacionAlumnoBean;
 import com.school.service.InformacionAlumnoService;
 
 public class InformacionAlumnoServiceImpl implements InformacionAlumnoService {
+
+	@Override
+	public boolean saveInformacionAlumno(InformacionAlumnoBean informacionalumnoBean) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateInformacionAlumno(InformacionAlumnoBean informacionalumnoBean) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public InformacionAlumnoBean getInformacionAlumno(int idInformacionAlumno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<InformacionAlumnoBean> getAllInformacionAlumno() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteInformacionAlumno(int idInformacionAlumno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
