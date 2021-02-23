5d9d1370eb15c36decbc56685fc3a662c9796db0
Jeremy <eremy95.ramos@gmail.com>
2021-02-23 10:32:08 -0600

    repository AlumnoInformacion

---
+++ School/src/main/java/com/school/repository/InformacionAlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.school.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.school.model.InformacionAlumno;
+
+public interface InformacionAlumnoRepository extends JpaRepository<InformacionAlumno, Integer> {
+
+}
