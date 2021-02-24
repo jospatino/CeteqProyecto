01c883119db80269b3816b68d93d26c5844a79c4
Daniel <daniel.hernandez8020@gmail.com>
2021-02-24 11:12:05 -0600

    Profe Implement y modificacion variable ProfeService

--- School/src/main/java/com/school/serviceimpl/ProfeServiceImpl.java
+++ School/src/main/java/com/school/serviceimpl/ProfeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.school.serviceimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,45 +8,100 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.school.bean.AlumnoBean;
+import com.school.bean.ProfesorBean;
+import com.school.model.ProfesorModel;
 import com.school.repository.ProfesorRepository;
-import com.school.service.AlumnoService;
+import com.school.service.ProfesorService;
+
 
 @Service
 @Transactional
-public class ProfeServiceImpl implements AlumnoService {
+public class ProfeServiceImpl implements ProfesorService {
 
 	@Autowired
 	public ProfesorRepository profeRepo;
+
+	@Override
+	public Integer saveProfesor(ProfesorBean ProfesorIngresado) {
+
+		ProfesorModel profesorNuevo = new ProfesorModel();
+		
+		profesorNuevo.setNombreProfesor(ProfesorIngresado.getNombreProfesor());
+		profesorNuevo.setAppProfesor(ProfesorIngresado.getAppProfesor());
+		profesorNuevo.setApmProfesor(ProfesorIngresado.getApmProfesor());
+		profesorNuevo.setId(ProfesorIngresado.getId());
+		
+		profeRepo.save(profesorNuevo);
+		
+		return profesorNuevo.getId();
+	}
+
+	@Override
+	public List<ProfesorBean> Profesorlist() {
+		List<ProfesorModel> profesorListModel = profeRepo.findAll();
+		List<ProfesorBean> profesorListBean = new ArrayList<ProfesorBean>();
+		
+		
+		for (ProfesorModel profesorModel : profesorListModel) {
+			
+			ProfesorBean profesorBean = new ProfesorBean();
+			
+			profesorBean.setNombreProfesor(profesorModel.getNombreProfesor());
+			profesorBean.setAppProfesor(profesorModel.getAppProfesor());
+			profesorBean.setApmProfesor(profesorModel.getApmProfesor());
+			profesorBean.setId(profesorModel.getId());
+			
+			profesorListBean.add(profesorBean);
+			
+		}
+		
+		return profesorListBean;
+	}
+
+	@Override
+	public ProfesorBean ProfesorbyId(Integer id_profesor) {
+		ProfesorModel profesorById = profeRepo.findById(id_profesor).orElse(null);
+		ProfesorBean profesorBean =  new ProfesorBean();
+		
+		profesorBean.setNombreProfesor(profesorById.getNombreProfesor());
+		profesorBean.setAppProfesor(profesorById.getAppProfesor());
+		profesorBean.setApmProfesor(profesorById.getApmProfesor());
+		profesorBean.setId(profesorById.getId());
+		
+		
+		
+		return profesorBean;
+	}
+
 	
 	@Override
-	public Integer saveAlumno(AlumnoBean alumnoBean) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean uptdateProfesor(ProfesorBean UpdateProfesor) {
+		
+		ProfesorModel profesor = profeRepo.findById(UpdateProfesor.getId()).orElseThrow(null);
+		
+		profesor.setNombreProfesor(UpdateProfesor.getNombreProfesor());
+		profesor.setAppProfesor(UpdateProfesor.getAppProfesor());
+		profesor.setApmProfesor(UpdateProfesor.getApmProfesor());
+		
+		profeRepo.save(profesor);
+		
+		
+		return true;
 	}
 
 	@Override
-	public List<AlumnoBean> list() {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean deleteProfesor(Integer id_profesor) {
+		
+		ProfesorModel profesorById = profeRepo.findById(id_profesor).orElse(null);
+		
+		profeRepo.delete(profesorById);
+		
+		
+		return true;
 	}
 
-	@Override
-	public AlumnoBean findIdAlumno(Integer idAlumnos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
 
-	@Override
-	public Boolean updateAlumno(AlumnoBean alumnosBean) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Boolean deleteById(Integer idAlumnos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
