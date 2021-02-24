757fd9f744129c6d15e46a27c4636d5cef31f3dc
Alex082 <a_ramirex@outlook.com>
2021-02-24 10:45:44 -0600

    Cambios Kardex Impl-Controller

--- School/src/main/java/com/school/serviceimpl/KardexServiceImpl.java
+++ School/src/main/java/com/school/serviceimpl/KardexServiceImpl.java
@@ -1,40 +1,80 @@
 package com.school.serviceimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.school.bean.InformacionAlumnoBean;
-import com.school.service.InformacionAlumnoService;
+import javax.transaction.Transactional;
 
-public class KardexServiceImpl implements InformacionAlumnoService {
+import org.springframework.stereotype.Service;
+
+import com.school.bean.KardexBean;
+import com.school.model.KardexModel;
+import com.school.repository.KardexRepository;
+import com.school.service.KardexService;
+
+@Service
+@Transactional
+public class KardexServiceImpl implements KardexService {
+	
+	
+	private KardexRepository kardexRepository;
 
 	@Override
-	public boolean saveInformacionAlumno(InformacionAlumnoBean informacionalumnoBean) {
-		// TODO Auto-generated method stub
-		return false;
+	public Integer saveKardex(KardexBean kardexBean) {
+		KardexModel kardexModel = new KardexModel();
+		kardexModel.setAño_kardex(kardexBean.getAño_kardex());
+		kardexModel.setCalificacion_kardex(kardexBean.getCalificacion_kardex());
+		
+		kardexRepository.save(kardexModel);
+		return kardexModel.getIdkardex();
 	}
 
 	@Override
-	public boolean updateInformacionAlumno(InformacionAlumnoBean informacionalumnoBean) {
-		// TODO Auto-generated method stub
-		return false;
+	public List<KardexBean> findKardex() {
+		List<KardexModel> kardexModelList = this.kardexRepository.findAll();
+		List<KardexBean> kardexBeanList = new ArrayList<KardexBean>();
+		
+		for(KardexModel kardexModel : kardexModelList) {
+			KardexBean kardexBean = new KardexBean();
+			kardexBean.setIdkardex(kardexModel.getIdkardex());
+			kardexBean.setAño_kardex(kardexModel.getAño_kardex());
+			kardexBean.setCalificacion_kardex(kardexModel.getCalificacion_kardex());
+			
+			kardexBeanList.add(kardexBean);
+		}
+		return kardexBeanList;
 	}
 
 	@Override
-	public InformacionAlumnoBean getInformacionAlumno(int idInformacionAlumno) {
-		// TODO Auto-generated method stub
-		return null;
+	public KardexBean findIdKardex(Integer idKardex) {
+		KardexModel kardexModel = kardexRepository.findById(idKardex).orElseThrow();
+		KardexBean kardexBean = new KardexBean();
+		
+		kardexBean.setAño_kardex(kardexModel.getAño_kardex());
+		kardexBean.setCalificacion_kardex(kardexModel.getCalificacion_kardex());
+		kardexBean.setIdkardex(kardexModel.getIdkardex());
+		
+		return kardexBean;
 	}
 
 	@Override
-	public List<InformacionAlumnoBean> getAllInformacionAlumno() {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean updateKardex(KardexBean kardexBean) {
+        KardexModel kardexModel = this.kardexRepository.findById(kardexBean.getIdkardex()).orElseThrow();
+		
+        kardexModel.setIdkardex(kardexBean.getIdkardex());
+		kardexModel.setAño_kardex(kardexBean.getAño_kardex());
+		kardexModel.setCalificacion_kardex(kardexBean.getCalificacion_kardex());
+		
+		this.kardexRepository.save(kardexModel);
+		return true;
 	}
 
 	@Override
-	public String deleteInformacionAlumno(int idInformacionAlumno) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean deleteByIdKardex(Integer idKardex) {
+		KardexModel kardexModel = kardexRepository.findById(idKardex).orElseThrow();
+		kardexRepository.delete(kardexModel);
+		return true;
 	}
+
 
 }
--- School/src/main/java/com/school/serviceimpl/TutorServiceImpl.java
+++ School/src/main/java/com/school/serviceimpl/TutorServiceImpl.java
@@ -1,7 +1,79 @@
 package com.school.serviceimpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.school.bean.TutorBean;
+import com.school.model.TutorModel;
+import com.school.repository.TutorRepository;
 import com.school.service.TutorService;
 
+@Service
+@Transactional
 public class TutorServiceImpl implements TutorService {
+	@Autowired
+	private TutorRepository tutorRepository;
+	
+	@Override
+	public Integer saveTutor(TutorBean tutorBean) {
+		TutorModel tutorModel = new TutorModel();
+		tutorModel.setNombreTutor(tutorBean.getNombreTutor());
+		tutorModel.setCorreo(tutorBean.getCorreo());
+		
+		tutorRepository.save(tutorModel);
+		return tutorModel.getId();
+	}
+
+	@Override
+	public List<TutorBean> findTutor() {
+		List<TutorModel> tutorModelList = this.tutorRepository.findAll();
+		List<TutorBean> tutorBeanList = new ArrayList<TutorBean>();
+		
+		for(TutorModel tutorModel : tutorModelList) {
+			TutorBean tutorBean = new TutorBean();
+			tutorBean.setId(tutorModel.getId());
+			tutorBean.setNombreTutor(tutorModel.getNombreTutor());
+			tutorBean.setCorreo(tutorModel.getCorreo());
+			
+			tutorBeanList.add(tutorBean);
+		}
+		return tutorBeanList;
+	}
+
+	@Override
+	public TutorBean findIdTutor(Integer idTutor) {
+		TutorModel tutorModel = tutorRepository.findById(idTutor).orElseThrow();
+		TutorBean tutorBean = new TutorBean();
+		
+		tutorBean.setId(tutorModel.getId());
+		tutorBean.setNombreTutor(tutorModel.getNombreTutor());
+		tutorBean.setCorreo(tutorModel.getCorreo());
+		
+		return tutorBean;
+	}
+
+	@Override
+	public Boolean updateTutor(TutorBean tutorBean) {
+		TutorModel tutorModel = this.tutorRepository.findById(tutorBean.getId()).orElseThrow();
+		
+		tutorModel.setId(tutorBean.getId());
+		tutorModel.setNombreTutor(tutorBean.getNombreTutor());
+		tutorModel.setCorreo(tutorBean.getCorreo());
+		
+		this.tutorRepository.save(tutorModel);
+		return true;
+	}
+
+	@Override
+	public Boolean deleteByIdTutor(Integer idTutor) {
+		TutorModel tutorModel = tutorRepository.findById(idTutor).orElseThrow();
+		tutorRepository.delete(tutorModel);
+		return true;
+	}
 
 }
