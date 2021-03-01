ffd794bbfa259be18637cf7bccabd51b8858ecc2
DiegoViag <diegovielma14@gmail.com>
2021-03-01 13:11:03 -0600

    Repository cliente

--- bancoCetq/src/main/java/com/banco/bean/ClienteBean.java
+++ bancoCetq/src/main/java/com/banco/bean/ClienteBean.java
@@ -1,5 +1,81 @@
 package com.banco.bean;
 
+import java.util.Date;
+
 public class ClienteBean {
 
+private Integer idCliente;
+	
+
+	private String nombreCliente;
+	
+
+	private String apPaterno;
+	
+
+	private String apMaterno;
+	
+
+	private Date fechaNacimiento;
+	
+
+	private String email;
+
+	public ClienteBean() {
+	}
+
+	public ClienteBean(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+
+	public String getApPaterno() {
+		return apPaterno;
+	}
+
+	public void setApPaterno(String apPaterno) {
+		this.apPaterno = apPaterno;
+	}
+
+	public String getApMaterno() {
+		return apMaterno;
+	}
+
+	public void setApMaterno(String apMaterno) {
+		this.apMaterno = apMaterno;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 }
--- bancoCetq/src/main/java/com/banco/repository/ClienteRepository.java
+++ bancoCetq/src/main/java/com/banco/repository/ClienteRepository.java
@@ -1,5 +1,11 @@
 package com.banco.repository;
 
-public interface ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.banco.model.ClienteModel;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteModel, Integer> {
 
 }
