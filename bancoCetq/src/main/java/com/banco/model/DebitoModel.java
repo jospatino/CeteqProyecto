b78b170a46f87a97d60c5dea8df8951f54684f4e
Jeremy <eremy95.ramos@gmail.com>
2021-03-01 13:17:38 -0600

    DebitoModel

--- bancoCetq/src/main/java/com/banco/model/DebitoModel.java
+++ bancoCetq/src/main/java/com/banco/model/DebitoModel.java
@@ -1,5 +1,84 @@
 package com.banco.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "debito")
+
 public class DebitoModel {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer idDebito;
+	
+	@Column (name = "idCliente", nullable = false)
+	private ClienteModel idCliente;
+	
+	@Column (name = "saldo", nullable = false)
+	private DebitoModel saldo;
+	
+	@Column (name = "idCredito", nullable = false)
+	private CreditoModel idCredito;
+
+	public DebitoModel() {
+	
+	}
+	
+	public DebitoModel(Integer idDebito) {
+		
+		this.idDebito = idDebito;
+	}
+	public Integer getIdDebito() {
+		return idDebito;
+	}
+
+	public void setIdDebito(Integer idDebito) {
+		
+		this.idDebito = idDebito;
+	}
+
+	public ClienteModel getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(ClienteModel idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public DebitoModel getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(DebitoModel saldo) {
+		this.saldo = saldo;
+	}
+
+	public CreditoModel getIdCredito() {
+		return idCredito;
+	}
+
+	public void setIdCredito(CreditoModel idCredito) {
+		this.idCredito = idCredito;
+	}
+
+	@Override
+	public String toString() {
+		return "DebitoModel [idDebito=" + idDebito + ", idCliente=" + idCliente + ", saldo=" + saldo + ", idCredito="
+				+ idCredito + "]";
+	}
+	
+	
+	
+	
+
+
+
 
 }
+	
+	
+	
+
+
+
