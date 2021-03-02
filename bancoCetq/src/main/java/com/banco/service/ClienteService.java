fbb90de42e050d9affbf6b2a2d7deab25c515f59
DiegoViag <diegovielma14@gmail.com>
2021-03-02 13:29:24 -0600

    Service

--- bancoCetq/src/main/java/com/banco/service/ClienteService.java
+++ bancoCetq/src/main/java/com/banco/service/ClienteService.java
@@ -1,5 +1,14 @@
 package com.banco.service;
 
+import java.util.List;
+
+import com.banco.bean.ClienteBean;
+
 public interface ClienteService {
 
+	public Integer save (ClienteBean clienteBean);
+	public List<ClienteBean> find();
+	public ClienteBean findId (Integer idCliente);
+	public Boolean update (ClienteBean clienteBean);
+	public Boolean delete (Integer idCliente);
 }
