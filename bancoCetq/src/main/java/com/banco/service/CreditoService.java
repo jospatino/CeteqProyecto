19645bd8a03dde0b4912b474319023869e26d857
Daniel <daniel.hernandez8020@gmail.com>
2021-03-03 14:53:44 -0600

    CreditoService

---
+++ bancoCetq/src/main/java/com/banco/service/CreditoService.java
@@ -0,0 +1,38 @@
+package com.banco.service;
+
+import java.util.List;
+
+import com.banco.bean.CreditoBean;
+
+
+
+public interface CreditoService {
+
+	
+	//create
+		public Integer crearCredito(CreditoBean creditoNuevo);
+		
+		//read
+		public List<CreditoBean> listaCreditos(); //muestra la lista
+		public CreditoBean creditoPorId(Integer idCredito); //devuelve un alumno por id
+		
+		//update devuelve true si se actualizo
+		public Boolean updateCredito(CreditoBean creditoBean);
+		
+		//delete por id
+		public Boolean deleteCreditoById(Integer idCredito);
+		
+		
+		//eligibilidad Prestamo
+		public Boolean eligibilidadPrestamo(Integer idCredito);
+		
+		//Cliente selecto
+		public List<CreditoBean> clientesSelectos();
+		
+		
+		// Upgrade cliente
+		public Boolean upgradeCliente(Integer idCredito);
+
+	
+	
+}
